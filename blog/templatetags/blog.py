d33e4c67cf0f72f489fc39b55d93df80e9ef8569
nathan@playgroundblues.com <nathan@playgroundblues.com@5ae32e2a-df40-0410-a01d-45158a99d8df>
2008-04-23 01:30:01 +0000

    Added inlines app and removed the render_inlines filter from the blog app.
    git-svn-id: http://django-basic-apps.googlecode.com/svn/trunk@25 5ae32e2a-df40-0410-a01d-45158a99d8df

--- blog/templatetags/blog.py
+++ blog/templatetags/blog.py
@@ -1,10 +1,6 @@
 from django import template
 from django.conf import settings
-from django.core import template_loader
 from django.db import models
-from django.contrib.contenttypes.models import ContentType
-from django.core.exceptions import ObjectDoesNotExist
-from django.utils.safestring import mark_safe
 import re
 Post = models.get_model('blog', 'post')
 Category = models.get_model('blog', 'category')
@@ -84,107 +80,6 @@ def do_get_blog_categories(parser, token):
     raise template.TemplateSyntaxError, "%s tag had invalid arguments" % tag_name
   var_name = m.groups()[0]
   return BlogCategories(var_name)
-
-#
-# Render Inlines (filter)
-#
-@register.filter
-def render_inlines(value):
-  """
-  Renders inlines in a ``Post`` by passing them through inline templates.
-  
-  Template Syntax::
-    
-    {{ post.body|render_inlines|markdown }}
-  
-  Inline Syntax (singular)::
-    
-      <inline type="<app_name>.<model_name>" id="<id>" class="med_left"></inline>
-    
-  Inline Syntax (plural)::
-  
-      <inline type="<app_name>.<model_name>" ids="<id>, <id>, <id>"></inline>
-  
-  An inline template will be used to render the inline. Templates will be
-  locaed in the following maner:
-  
-    ``inlines/<app_name>_<model_name>.html``
-  
-  The template will be passed the following context:
-    
-    ``object``
-      An object for the corresponding passed id.
-  
-  or
-  
-    ``object_list``
-      A list of objects for the corresponding ids.
-  
-  It would be wise to anticipate both object_list and object unless 
-  you know for sure one or the other will only be present.
-  """
-  try:
-    try:
-      from BeautifulSoup import BeautifulSoup
-    except ImportError:
-      from beautifulsoup import BeautifulSoup
-  
-    soup = BeautifulSoup(value)
-    inline_list = soup.findAll('inline')
-    inline_context = {}
-  
-    for inline in inline_list:
-      try:
-        app_label, model_name = inline['type'].split('.')
-        content_type = ContentType.objects.get(app_label=app_label, model=model_name)
-        Model = content_type.model_class()
-        try:
-          inline_class = inline['class']
-        except:
-          inline_class = ''
-          
-        try:
-          id_list = [int(i) for i in inline['ids'].split(',')]
-          object_list = Model.objects.in_bulk(id_list)
-          object_list = list(object_list[int(i)] for i in id_list)
-          inline_context = template.Context({ 'object_list': object_list, 'settings':settings, 'class':inline_class })
-          inline_template = 'inlines/%s_%s.html' % (content_type.app_label, content_type.model)
-        except KeyError:
-          obj = Model.objects.get(pk=inline['id'])
-          inline_context = template.Context({ 'object': obj, 'settings':settings, 'class':inline_class })
-          inline_template = 'inlines/%s_%s.html' % (content_type.app_label, content_type.model)
-      except template.TemplateDoesNotExist:
-        inline_context = {'error':'Template for "%s" does not exist.' % app_label }
-        inline_template = 'inlines/missing.html'
-      except ContentType.DoesNotExist:
-        inline_context = {'error':'Content type does not exist.'}
-        inline_template = 'inlines/missing.html'
-      except KeyError:
-        inline_context = {'error':'Be sure there are both "type" and "id" attributes in the inline tag.'}
-        inline_template = 'inlines/missing.html'
-      except ValueError:
-        inline_context = {'error':'Type "%s" does not exist' % inline['type'] }
-        inline_template = 'inlines/missing.html'
-      except Model.DoesNotExist:
-        inline_context = {'error':'Object does not exist.' }
-        inline_template = 'inlines/missing.html'
-      
-      try:
-        if inline_context['error']:
-          if settings.DEBUG is True:
-            rendered_inline = template_loader.render_to_string(inline_template, inline_context)
-            inline.replaceWith(rendered_inline)
-          else:
-            inline.replaceWith('')
-      except:
-        rendered_inline = template_loader.render_to_string(inline_template, inline_context)
-        inline.replaceWith(rendered_inline)
-    return mark_safe(soup)
-  except ImportError:
-    return mark_safe(value)
-
-render_inlines.is_safe = True
-
 
 @register.filter
 def get_links(value):
---
+++ inlines/templatetags/inlines.py
@@ -0,0 +1,41 @@
+from django import template
+from basic.inlines.parser import inlines
+
+register = template.Library()
+
+@register.filter
+def render_inlines(value):
+  """
+  Renders inlines in a ``Post`` by passing them through inline templates.
+  
+  Template Syntax::
+    
+    {{ post.body|render_inlines|markdown }}
+  
+  Inline Syntax (singular)::
+    
+      <inline type="<app_name>.<model_name>" id="<id>" class="med_left" />
+    
+  Inline Syntax (plural)::
+  
+      <inline type="<app_name>.<model_name>" ids="<id>, <id>, <id>" />
+  
+  An inline template will be used to render the inline. Templates will be
+  locaed in the following maner:
+  
+    ``inlines/<app_name>_<model_name>.html``
+  
+  The template will be passed the following context:
+    
+    ``object``
+      An object for the corresponding passed id.
+  
+  or
+  
+    ``object_list``
+      A list of objects for the corresponding ids.
+  
+  It would be wise to anticipate both object_list and object unless 
+  you know for sure one or the other will only be present.
+  """
+  return inlines(value)
