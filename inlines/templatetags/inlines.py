3f32ab480b5309969930db6aa35f0aeab441a442
nathan@playgroundblues.com <nathan@playgroundblues.com@5ae32e2a-df40-0410-a01d-45158a99d8df>
2008-04-24 01:21:01 +0000

    Added an admin interface to the blog app so adding inlines is easier. Also added an InlineType model to the inlines app so you can be more selective about what content type is allowed to be an inline.
    git-svn-id: http://django-basic-apps.googlecode.com/svn/trunk@29 5ae32e2a-df40-0410-a01d-45158a99d8df

--- inlines/templatetags/inlines.py
+++ inlines/templatetags/inlines.py
@@ -1,5 +1,7 @@
 from django import template
 from basic.inlines.parser import inlines
+from basic.inlines.models import InlineType
+import re
 
 register = template.Library()
 
@@ -43,3 +45,38 @@ def render_inlines(value):
 @register.filter
 def extract_inlines(value):
   return inlines(value, True)
+
+#
+# Get inline types
+#
+class InlineTypes(template.Node):
+  def __init__(self, var_name):
+    self.var_name = var_name
+
+  def render(self, context):
+    types = InlineType.objects.all()
+    context[self.var_name] = types
+    return ''
+
+@register.tag(name='get_inline_types')
+def do_get_inline_types(parser, token):
+  """
+  Gets all inline types.
+
+  Syntax::
+
+    {% get_inline_types as [var_name] %}
+
+  Example usage::
+
+    {% get_inline_types as inline_list %}
+  """
+  try:
+    tag_name, arg = token.contents.split(None, 1)
+  except ValueError:
+    raise template.TemplateSyntaxError, "%s tag requires arguments" % token.contents.split()[0]
+  m = re.search(r'as (\w+)', arg)
+  if not m:
+    raise template.TemplateSyntaxError, "%s tag had invalid arguments" % tag_name
+  var_name = m.groups()[0]
+  return InlineTypes(var_name)
