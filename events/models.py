6525675e1f582094a08f644d0748cc308de6845f
nathan@playgroundblues.com <nathan@playgroundblues.com@5ae32e2a-df40-0410-a01d-45158a99d8df>
2008-09-10 19:43:58 +0000

    Throwing basic.events out there just to get the ball rolling.
    git-svn-id: http://django-basic-apps.googlecode.com/svn/trunk@69 5ae32e2a-df40-0410-a01d-45158a99d8df

---
+++ events/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.db.models import permalink
+from django.contrib.auth.models import User
+from tagging.fields import TagField
+from basic.places.models import Place
+
+
+class Event(models.Model):
+    """ Event model """
+    title           = models.CharField(max_length=200)
+    slug            = models.SlugField()
+    place           = models.ForeignKey(Place, blank=True, null=True)
+    one_off_place   = models.CharField(max_length=200, blank=True)
+    description     = models.TextField(blank=True)
+    submitted_by    = models.ForeignKey(User, blank=True, null=True)
+    tags            = TagField()
+    created         = models.DateTimeField(auto_now_add=True)
+    modified        = models.DateTimeField(auto_now=True)
+  
+    class Meta:
+        verbose_name = _('event')
+        verbose_name_plural = _('events')
+        db_table = 'events'
+
+    def __unicode__(self):
+        return self.title
+    
+    @permalink
+    def get_absolute_url(self):
+        return ('event_detail', None, {
+            'year': self.publish.year,
+            'month': self.publish.strftime('%b').lower(),
+            'day': self.publish.day,
+            'slug': self.slug,
+            'event_id': self.id
+        })
+
+
+class EventTime(models.Model):
+    """ EventTime model """
+    event           = models.ForeignKey(Event, related_name='event_times')
+    start           = models.DateTimeField()
+    end             = models.DateTimeField(blank=True)
+    is_all_day      = models.BooleanField(default=False)
+  
+    class Meta:
+        verbose_name = _('event time')
+        verbose_name_plural = _('event times')
+        db_table = 'event_times'
+
+    def __unicode__(self):
+        return u'<Event Time>'
