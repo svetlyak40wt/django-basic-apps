2c3963d22d7c560d27778ffef54b885ac095b8d7
nathan@playgroundblues.com <nathan@playgroundblues.com@5ae32e2a-df40-0410-a01d-45158a99d8df>
2008-10-14 05:02:57 +0000

    Added email notification to follow view
    git-svn-id: http://django-basic-apps.googlecode.com/svn/trunk@85 5ae32e2a-df40-0410-a01d-45158a99d8df

--- relationships/views.py
+++ relationships/views.py
@@ -4,18 +4,30 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.db import models
+from django.core.mail import EmailMessage
+from django.conf import settings
 
 Relationship = models.get_model('relationships', 'relationship')
 
 
 @login_required
-def follow(request, to_user_id, template_name='relationships/relationship_add_confirm.html', success_template_name='relationships/relationship_add_success.html', mimetype='text/html'):
+def follow(request, to_user_id, template_name='relationships/relationship_add_confirm.html', success_template_name='relationships/relationship_add_success.html', email_template='relationships/follow_email.txt', mimetype='text/html'):
     to_user = get_object_or_404(User, pk=to_user_id)
     from_user = request.user
     
     if request.is_ajax() or request.POST:
         relationship = Relationship(from_user=from_user, to_user=to_user)
         relationship.save()
+        
+        if not settings.DEBUG:
+            context = {
+                'from_user': from_user, 
+                'to_user': to_user
+            }
+            subject =  '%s is now following you' % from_user.username
+            message = render_to_string(email_template, context)
+            email = EmailMessage(subject, message, settings.REPLY_EMAIL, ['%s' % to_user.email])
+            email.send(fail_silently=False)
         
         if request.is_ajax():
             context = "{'success': 'Success', 'to_user_id': '%s'}" % (to_user.id)
