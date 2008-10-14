ceab96a723600023c67934227e212076d511dd16
nathan@playgroundblues.com <nathan@playgroundblues.com@5ae32e2a-df40-0410-a01d-45158a99d8df>
2008-10-14 05:04:15 +0000

    Forgot to import render_to_string
    git-svn-id: http://django-basic-apps.googlecode.com/svn/trunk@86 5ae32e2a-df40-0410-a01d-45158a99d8df

--- relationships/views.py
+++ relationships/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.template.loader import render_to_string
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
